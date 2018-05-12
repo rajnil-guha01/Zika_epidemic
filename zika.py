c8f33b6072e8b3040368be6d47232e956d633f1a
rajnil-guha01 <rajnil94.guha@gmail.com>
2018-05-12 14:34:25 +0530

    adding original dataset, data cleaning python script and cleaned dataset

---
+++ zika.py
@@ -0,0 +1,21 @@
+import csv
+
+
+def modify_zika():
+    open_csv = open('cdczika.csv', 'r')
+    write_csv = open('newzika.csv', 'w')
+
+    csv_reader = csv.reader(open_csv)
+    csv_writer = csv.writer(write_csv)
+
+    next(csv_reader)
+
+    for line in csv_reader:
+        if line[7].startswith('"'):
+            line[7] = line[7][1:]
+        if line[7].endswith('"'):
+            line[7] = line[7][:-1]
+        csv_writer.writerow(line)
+
+    open_csv.close()
+    write_csv.close()
